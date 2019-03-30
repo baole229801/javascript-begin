23442c04682f392b8939906135f3790cb9bbb5dc
baoletrg <baole2298@gmail.com>
2019-03-30 20:36:06 +0700

    add student average score method

--- stdexercise/student.js
+++ stdexercise/student.js
@@ -12,4 +12,8 @@ Student.prototype.ask = function() {
 	this.gender = readlineSync.question('Gender: ');
 };
 
+Student.prototype.averageScore = function(first_argument) {
+	this.score = 10;
+};
+
 module.exports = Student;
